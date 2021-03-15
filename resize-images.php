94b908a39a74679039790c4be888a85db36b8e9c
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-14 21:19:10 -0300

    chore: resize images single thread example

---
+++ resize-images.php
@@ -0,0 +1,33 @@
+<?php
+
+use Alura\Threads\Student\InMemoryStudentRepository;
+use parallel\Runtime;
+
+require_once 'vendor/autoload.php';
+
+$repository = new InMemoryStudentRepository();
+$studentList = $repository->all();
+
+foreach ($studentList as $student) {
+  echo 'Resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
+
+  resizeTo200PixelsWidth($student->profilePicturePath());
+
+  echo 'Finishing resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
+}
+
+function resizeTo200PixelsWidth($imagePath)
+{
+    [$width, $height] = getimagesize($imagePath);
+
+    $ratio = $height / $width;
+
+    $newWidth = 200;
+    $newHeight = 200 * $ratio;
+
+    $sourceImage = imagecreatefromjpeg($imagePath);
+    $destinationImage = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($destinationImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+    imagejpeg($destinationImage, __DIR__ . '/storage/resized/' . basename($imagePath));
+}
