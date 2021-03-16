664ae80f0b907df93aff9632360762dcc960e974
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-15 21:34:19 -0300

    chore: calculating student points

---
+++ calculate-points.php
@@ -0,0 +1,24 @@
+<?php
+
+use Alura\Threads\Activity\Activity;
+use Alura\Threads\Student\InMemoryStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$repository = new InMemoryStudentRepository();
+$studentList = $repository->all();
+
+$totalPoints = 0;
+foreach ($studentList as $student) {
+  $activities = $repository->activitiesInADay($student);
+
+  $totalPoints += $points = array_reduce(
+      $activities,
+      fn (int $total, Activity $activity) => $total + $activity->points(),
+      0
+  );
+
+  printf('%s made %d points today%s', $student->fullName(), $points, PHP_EOL);
+}
+
+printf('We had a total of %d points today%s', $totalPoints, PHP_EOL);
