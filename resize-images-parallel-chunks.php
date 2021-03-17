deebe8b9700354921d011d647a0282c59b5df653
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-16 21:47:51 -0300

    chore: add parallel chunk file

---
+++ resize-images-parallel-chunks.php
@@ -0,0 +1,46 @@
+<?php
+
+use parallel\Runtime;
+use Alura\Threads\Student\Student;
+use Alura\Threads\Student\InMemoryStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$start = microtime(true);
+
+$repository = new InMemoryStudentRepository();
+$studentList = $repository->all();
+
+$runtimes = [];
+foreach ($studentList as $i => $student) {
+
+  $runtimes[$i] = new Runtime('vendor/autoload.php');
+  $runtimes[$i]->run(function (Student $student) {
+    echo 'Resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
+
+    $profilePicturePath = $student->profilePicturePath();
+    [$width, $height] = getimagesize($profilePicturePath);
+
+    $ratio = $height / $width;
+
+    $newWidth = 200;
+    $newHeight = 200 * $ratio;
+
+    $sourceImage = imagecreatefromjpeg($profilePicturePath);
+    $destinationImage = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($destinationImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+    imagejpeg($destinationImage, __DIR__ . '/storage/resized/' . basename($profilePicturePath));
+
+    echo 'Finishing resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
+  }, [$student]);
+}
+
+foreach ($runtimes as $runtime) {
+  $runtime->close();
+}
+
+echo 'Finishing main thread' . PHP_EOL;
+
+$time_elapsed_secs = microtime(true) - $start;
+echo $time_elapsed_secs . PHP_EOL;
